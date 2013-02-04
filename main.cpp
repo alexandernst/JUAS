c46ac5f0b6711a2d200f78ed6e72bbcdb65b9a25
Alexander Nestorov <alexandernst@gmail.com>
2013-02-04 13:06:05 +0100

    No need for singleton (special use-cases)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <QtWidgets/QApplication>
 #include "webwidget.h"
 
-int main(int argc, char *argv[])
-{
-    //TODO: make singleton
+int main(int argc, char *argv[]){
     QApplication app(argc, argv);
 
     app.setQuitOnLastWindowClosed(false);
