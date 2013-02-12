b9ae38c0d3b8fffd9ff439ed05a5ad2e4d84cf49
NachE <nache.nache@gmail.com>
2013-02-12 01:20:08 +0100

    changed demo style padding to 0px

--- html/js/libs/jquery-layout.js
+++ html/js/libs/jquery-layout.js
@@ -161,7 +161,7 @@ $.layout = {
                         }
                 ,	cssDemo: { // DEMO CSS - applied if: options.PANE.applyDemoStyles=true
                                 overflow:	"auto"
-                        ,	padding:	"10px"
+                        ,	padding:	"0px"
                         }
                 ,	cssDemoPane: { // DEMO CSS - REMOVE scrolling from 'pane' when it has a content-div
                                 overflow:	"hidden"
@@ -175,7 +175,7 @@ $.layout = {
                         //	$.layout.defaults.zIndexes.pane_normal
                         }
                 ,	cssDemo: { // DEMO CSS - applied if: options.PANE.applyDemoStyles=true
-                                padding:	"10px"
+                                padding:	"0px"
                         ,	background:	"#FFF"
                         ,	border:		"1px solid #BBB"
                         ,	overflow:	"auto"
